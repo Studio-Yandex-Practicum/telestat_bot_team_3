a6d8b76c56b5c52e46ba1fb87961d9507efd4337
orbikadm <126566356+orbikadm@users.noreply.github.com>
2024-04-30 00:51:15 +1000

    Update handlers.py
    Принты исправлены на логгирование

--- handlers.py
+++ handlers.py
@@ -17,23 +17,19 @@ reply_keyboard = ReplyKeyboardMarkup(keyboard=[
 
 ], one_time_keyboard=True, resize_keyboard=True)
 
-CHAT_ID: int = None
-
 
 @app.on_message(filters.command('start'))
 async def command_start(client: Client, message: messages_and_media.message.Message):
-    print('Проверка на авторизацию') # await authorization()
-    logger.info('Проверка на авторизацию')
-    logger.complete()
+    logger.info('Проверка на авторизацию') # await authorization()
     await client.send_message(message.chat.id, 'Вы прошли авторизацию!', reply_markup=reply_keyboard) #
     logger.debug('Вы прошли авторизацию!')
 
 
 @app.on_message(filters.text == 'начать сбор аналитики')
 async def collect_analitycs(client: Client, message: messages_and_media.message.Message):
-    print('Бот начал работу')
+    logger.info('Бот начал работу')
     await client.send_message(message.chat.id, '...Здесь идет активный сбор данных пользователей...')
-    print('...Здесь идет активный сбор данных пользователей...')
+    logger.info('...Здесь идет активный сбор данных пользователей...')
 
 
 app.run()
