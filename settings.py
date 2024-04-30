8be7a258a835424256602d10026b4caa18f30feb
orbikadm <126566356+orbikadm@users.noreply.github.com>
2024-04-30 19:51:09 +1000

    Update settings.py
    Добавлены тайпинг и докстринг к функции configure_logging

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from loguru import logger
+from loguru._logger import Logger
 
 
 load_dotenv('.env')
@@ -13,7 +14,18 @@ class Config(object):
     DB_URI = os.environ['DB_URI']
 
 
-def configure_logging():
+def configure_logging() -> Logger:
+    """
+    Функция-конфигуратор логгера.
+
+    Найстройки:
+    level - уровень логгирования сообщений. От указанного и выше.
+    rotation - объем файла, при достижении которого создается новый файл логов.
+    retention - время, через которое файл логов удаляется.
+    compression - сжатие файлов логов для хранения.
+    enqueue - использование логгера в асинхронном режиме.
+    """
+
     logger.add(
         'logs/file_{time}.log',
         level='DEBUG',
