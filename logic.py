67c2c95d46ce42a9117f871d4abc3fb1a500ebea
Krivolap Dmitriy Aleksandrovich <dk@4vrs.ru>
2024-05-16 04:34:35 +0700

    FloodWait bugfix.

--- logic.py
+++ logic.py
@@ -130,6 +130,7 @@ async def choise_channel(client, message):
             logger.error(f'У пользователя {Config.BOT_ACCOUNT_NAME} '
                          'слишком много контактов, сработала защита '
                          f'"Телеграм"\n {e}')
+            continue
         except ChatAdminRequired:
             logger.error(f'Пользователю: {Config.BOT_ACCOUNT_NAME} '
                          'требуются права администратора.')
