9b4b94b7c8fe9c49be863d4cd6d7bc50db2a197e
Krivolap Dmitriy Aleksandrovich <dk@4vrs.ru>
2024-05-19 06:18:28 +0700

    2 bots running in current time

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import logging
 import subprocess
 from threading import Thread
 
-from bot_1 import bot_1
-from bot_2 import bot_2
 from services.launcher import init_process
 from settings import configure_logging
 
@@ -26,27 +24,7 @@ logger = configure_logging()
 
 # init_process()
 
-# subprocess.Popen('python bot_1.py')
-# subprocess.Popen('python bot_2.py')
-
-# pool = mp.Pool(mp.cpu_count())
-# pool.apply_async(bot_1.run())
-
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(bot_2.run())
-# loop.run_forever()
-
-# pool = mp.Pool(mp.cpu_count())
-# def run_command():
-#     subprocess.run(bot_1.run())
-#     subprocess.run(bot_2.run())
-
-
 if __name__ == '__main__':
-
-    import multiprocessing as mp
-    import subprocess
-    from threading import Thread
 
     Thread(target=subprocess.run, args=['py bot_1.py']).start()
     Thread(target=subprocess.run, args=['py bot_2.py']).start()
